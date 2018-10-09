f8690afbdbf2828bab214a76eb8d0c2344bf9ae2
Cree <Cree.robin@gmail.com>
2018-10-09 17:25:25 -0400

    Movie class w/ properties

---
+++ Final Midterm/Movies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Midterm
+{
+    class Movies
+    {
+        public String MovieName { get; set; }
+        public String Genre { get; set; }
+        public String Actor { get; set; }
+        public String Director { get; set; }
+
+        public Movies (string MovieName, string Genre, string Actor, string Director)
+        {
+            this.MovieName = MovieName;
+            this.Genre = Genre;
+            this.Actor=Actor;
+            this.Director = Director;
+        }
+    }
+}
